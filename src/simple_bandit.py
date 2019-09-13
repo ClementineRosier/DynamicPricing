da5db8053f6f9b7058b80ab0178b60f66b3c8918
nicolasmion <nicolas.mion@ekimetrics.com>
2019-09-13 18:21:21 +0200

    Simple Bandit

--- src/simple_bandit.py
+++ src/simple_bandit.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import scipy
-from scipy.stats import truncnorm
+from scipy.stats import truncnorm, invgamma
 import math
 
 
@@ -10,21 +10,57 @@ class SimpleBandit():
     Implementation of a classic multi armed bandit problem for a price selection problem.
     Each arm is set to be a given price : the model estimates the reward probability for each arm
     We model reward probability as a gaussian distribution with parameters (mu, sigma)
+    P(mu|sigma) ~ Norm(alpha, sigma)
+    P(sigma) ~ InvGamma(a_0, b_0)
+
+    Following that modelisation : P(mu, sigma | R) ~= P(R | mu, sigma) * P(mu | sigma) * P(sigma)
+
     Action selection via Thompson sampling
 
     Args : 
-        k_mu(list) : list of size k (number of arms) defining the mean for the gaussian prior for each arm
-        k_sigma(list) : list of size k (number of arms) defining the std for the gaussian prior for each arm
         k_p(list) : list of size k (number of arms) defining the price for each arm
+        alpha_0 (list) : list of size k (number of arms)
+        beta_0 (list) : list of size k (number of arms)
+        a_0 (list): list of size k (number of arms)
+        b_0 (list): list of size k (number of arms)
     """
 
-    def __init__(self, k_mu, k_sigma, k_p):
-        assert len(k_mu) == len(k_sigma) == len(k_p), "k_mu, k_sigman and k_p must all be same length" 
-        self.k_mu = k_mu
-        self.k_sigma = k_sigma
+    def __init__(self, k_p, alpha_0, beta_0, a_0, b_0):
+
+        assert len(k_p) == len(alpha_0) == len(beta_0) == len(a_0) == len(b_0), "k_mu, k_sigman and k_p must all be same length" 
         self.k_p = k_p
-        self.k = len(self.k_mu)
+        self.k = len(self.k_p)
+        self.alpha_0 = np.array(alpha_0)
+        self.beta_0 = np.array(beta_0)
+        self.a_0 = np.array(a_0)
+        self.b_0 = np.array(b_0)
+        self.n_obs = np.repeat(0, self.k) # number of trials for each arm
+
+        self.alpha_n = self.alpha_0
+        self.a_n = self.a_0
+        self.b_n = self.b_0
+
         print(f"SimpleBandit model instanciated with {self.k} arms.")
+
+
+    def update(self, k, reward):
+        """
+        Update priors for arm k given observation of reward 
+
+        Args : 
+            k (int) : index of the arm played
+            reward (float) : value of the observed reward
+        """
+        n = self.n_obs[k]
+        beta_0 = self.beta_0[k] 
+        alpha_0 = self.alpha_0[k]
+        a_0 = self.a_0[k]
+        b_0 = self.b_0[k]
+
+        self.alpha_n[k] = 1/ ( n+1 / beta_0) * ( reward + 1/beta_0 * alpha_0)
+        self.a_n[k] = n + a_0
+        self.b_n[k] = 1 /self.a_n[k] * ( (reward - alpha_0 )**2 / (1 + beta_0) +  a_0/b_0)
+
 
     def thompson_sampling(self):
         """
@@ -32,7 +68,12 @@ class SimpleBandit():
         Return : 
             int : argmax over sampling
         """
-        return np.argmax([np.random.normal(loc = self.k_mu[i], scale = self.k_sigma[i]) for i in range(self.k)])
+        # Sample sigma
+        sigma = self.b_n * invgamma.rvs(self.a_n)
+        # Sample mu
+        mu = np.random.normal(self.alpha_n, sigma * self.beta_0)
+        return np.argmax(mu)
+
 
     def chose_action(self, method = "thompson", force_action = None):
         """
@@ -57,4 +98,3 @@ class SimpleBandit():
         elif method == "random":
             self.action = np.random.randint(0,self.k)
         return 
-
