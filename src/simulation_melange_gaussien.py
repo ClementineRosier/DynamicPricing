d29aebd214d42161b81729a07052fa78df9f5197
ClementineRosier <33656081+ClementineRosier@users.noreply.github.com>
2019-09-16 22:43:21 +0200

    simulations uniforme et somme de gaussienne

---
+++ src/simulation_melange_gaussien.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import numpy as np
+import scipy
+from scipy.stats import truncnorm
+import math
+
+class GMixtureSimulation():
+    """
+    Simple demand simulation class based on a sum of independant normal variables
+    nb: possibilité de mettre des poids sur les normales ou d'avoir plus de normale
+
+    Args : 
+        mu_1(float) : mean of the first normal
+        sigma_1(float) : std of the first normal
+        mu_2(float) : mean of the 2nd normal
+        sigma_2(float) : std of the 2nd normal
+    """
+    def __init__(self,mu_1,sigma_1,mu_2,sigma_2):
+        self.mu_1=mu_1
+        self.sigma_1=sigma_1
+        self.mu_2=mu_2
+        self.sigma_2=sigma_2
+        self.gm_mu, self.gm_sigma = self.gm()
+        self.optimal_price = self.get_optimal_price()
+        self.max_revenue = self.compute_revenue(self.optimal_price)
+               
+    def gm(self):
+        """
+        Compute mean and std for truncated normal
+        """
+        gm_mu = self.mu_1 +self.mu_2
+        gm_sigma = self.sigma_1 +self.sigma_2
+        return gm_mu, gm_sigma
+
+    def get_optimal_price(self):
+        """
+        Computes the optimal price given the underlying distribution
+        (optimal price is the price that maximizes revenue)
+        """
+        res = scipy.optimize.minimize(self.compute_revenue, self.mu, method='nelder-mead')
+        return res.x[0]
+
+    def compute_revenue(self, p):
+        return -p*(1-scipy.stats.norm.cdf(p,loc = self.gm_mu,scale=slef.gm_sigma))
+
+    def _simulate(self):
+        """
+        Hidden method for random sampling
+        """
+        s = -1
+        while s < 0:
+            s=np.random.normal( self.mu_1,self.sigma_1, 1)+np.random.normal( self.mu_2,self.sigma_2, 1)
+        return s
+
+    def evaluate(self,p):
+        """
+        Return bool : True if buy False either
+        """
+        sample = self._simulate()
+        return bool(sample >= p)
+    
+    
+
---
+++ src/simulation_uniforme.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import numpy as np
+import scipy
+from scipy.stats import truncnorm
+import math
+
+class UniformeSimulation():
+    """
+    Simple demand simulation class following a continuous uniform with support [a,b]
+
+    Args : 
+        a(float) : lowest value of the distribution (by default =0)
+        b(float) : largest value fo the distribution (by default =1)
+    """
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+        self.mu, self.sigma = self.mean_variance()
+        self.optimal_price = self.get_optimal_price()
+        self.max_revenue = self.compute_revenue(self.optimal_price)
+               
+    def mean_variance(self):
+        """
+        Compute mean and std for truncated normal
+        """
+        mu = (self.a +self.b)/2
+        sigma = (self.a -self.b)**2/12
+        return mu, sigma
+
+    def get_optimal_price(self):
+        """
+        Computes the optimal price given the underlying distribution
+        (optimal price is the price that maximizes revenue)
+        """
+        res = scipy.optimize.minimize(self.compute_revenue, self.mu, method='nelder-mead')
+        return res.x[0]
+
+    def compute_revenue(self, p):
+        if p<=self.a:
+            return -1*p
+        elif p >=self.b:
+            return 0
+        else:
+            return -p*(self.b-p)/(self.b -self.a)
+        
+
+    def _simulate(self):
+        """
+        Hidden method for random sampling
+        """
+        s = -1
+        while s < 0:
+            s=np.random.uniform( self.a,self.b, 1)
+        return s
+
+    def evaluate(self,p):
+        """
+        Return bool : True if buy False either
+        """
+        sample = self._simulate()
+        return bool(sample >= p)
+    
+    
+
